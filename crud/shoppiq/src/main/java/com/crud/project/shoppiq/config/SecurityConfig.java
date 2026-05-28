2b62a1b6a665dd4682e7213b7c1b6820c7580df8
prabhatkrmishra <mprabhat774@gmail.com>
2026-05-28 18:56:41 +0530

    shoppiq: Fix lombook and SecurityFilterChain signature

--- crud/shoppiq/src/main/java/com/crud/project/shoppiq/config/SecurityConfig.java
+++ crud/shoppiq/src/main/java/com/crud/project/shoppiq/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig {
 
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(csrf -> csrf.disable())
